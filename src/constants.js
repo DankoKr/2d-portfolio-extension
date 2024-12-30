70401beb665fff8d4a67d8709f87783a7f6ccdc5
DankoKr <dankokralski776@gmail.com>
2024-12-30 14:19:08 +0100

    feat: Game movement version 1

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,10 @@
 export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `PC text`,
+  tv: `TV text`,
+  resume: `CV`,
+  projects: `project text`,
+  library: `Library text`,
+  exit: `Exit text`,
+};
